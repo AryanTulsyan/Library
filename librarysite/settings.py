ab2131eefb3b0806e66e60f5802f16af2fb1da3b
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-12 00:07:21 +0530

    updated allowed hosts

--- librarysite/settings.py
+++ librarysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4b$$v+4$zys2^tttea&j_3=e1nfo=ddshq9_io4$%zb7s+6*t_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
