5c544c110b44ac22dd1343f5512816816cb38eb0
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-16 09:50:28 +0530

    Initial website

--- books/admin.py
+++ books/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 from .models import Book
 
 admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('book_name', 'author_name', 'quantity')
+    search_fields = ('book_name', 'author_name')
