64d6dec16a7bc4ecea64c814aaa2b9d0f73b4be3
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-19 14:30:04 +0530

    Prepare project for Render deployment

--- librarysite/settings.py
+++ librarysite/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'librarysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # <-- Change this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
