752f397d055e17a9c76aa4a62671ec2f4e8ba057
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-21 13:40:10 +0530

    Add page_count and location fields to Book model

--- books/models.py
+++ books/models.py
@@ -5,8 +5,9 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    isbn = models.CharField(max_length=20)
+    total_number_of_pages = models.IntegerField(null=True, blank=True)
     available = models.BooleanField(default=True)
+    location = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
         return self.title
