f344ab7a623324f52cd71499e402788beb359817
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-23 22:01:52 +0530

    Fix: Force Django to use APP_DIRS for templates

--- librarysite/settings.py
+++ librarysite/settings.py
@@ -56,10 +56,12 @@ ROOT_URLCONF = 'librarysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        # Keep DIRS empty so Django is forced to look inside app directories
+        'DIRS': [], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
