ef645d417bb40c20930259eb985c5792a67a1df3
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-23 10:01:53 +0530

    Fix: successfully stage and remove broken views import from urls

--- urls.py
+++ urls.py
@@ -5,9 +5,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # Route for your beautiful new Jazzmin admin dashboard
     path('admin/', admin.site.urls),
-    
-    # Route that connects your main domain to your books catalog pages
     path('', include('books.urls')),
 ]
