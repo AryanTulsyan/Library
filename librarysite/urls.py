7429cd05e4c6a6277bc2d24cc89fdde49266ac96
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-23 23:46:28 +0530

    Urls: Handle favicon routing to prevent serverless function crashes

--- librarysite/urls.py
+++ librarysite/urls.py
@@ -1,26 +1,11 @@
-"""
-URL configuration for librarysite project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/stable/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView # <-- Add this import
 
 urlpatterns = [
-    # Route for the Django admin dashboard
-    path('admin/', admin.site.urls),
+    # Catch favicon requests and safely redirect them to your static folder
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
     
-    # Connects your homepage and catalog urls from the 'books' app
-    path('', include('books.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('books.urls')), 
 ]
