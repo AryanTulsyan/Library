96729210643797fe0e4041e7f2e72a73c07b4019
Aryan Tulsyan <coltaryan@gmail.com>
2026-05-25 10:15:33 +0530

    Database: Add financial tracking attributes to Book model

--- books/models.py
+++ books/models.py
@@ -7,7 +7,10 @@ class Book(models.Model):
     total_number_of_pages = models.IntegerField(null=True, blank=True)
     available = models.BooleanField(default=True)
     location = models.CharField(max_length=10, null=True, blank=True)
-
+    borrow_fee = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    deposit_fee = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    payment_number = models.CharField(max_length=50, default="YOUR-PAYMENT-NUMBER-HERE")
+    
     def __str__(self):
         return self.title
 
